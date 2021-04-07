db6797b1265f3d83a892da201a676a3b1e816731
aheinricher <aheinricher@gmail.com>
2021-04-07 17:34:09 -0400

    Added in-line comments to make module

--- pdftex.mk
+++ pdftex.mk
@@ -10,28 +10,71 @@ PDFTEX=pdflatex
 # Wildcard expressions for cleaning up after pdflatex
 PDFTEX_CLEAN_PATHS=*.aux *.pdf *.log
 
-# User-defined options for latex.
-# Override before including this file to change it.
-PDFTEX_USER_FLAGS=
+# Search paths for classes and packages.
 
-# Mode for running PDFTEX
-# Override to change.
-PDFTEX_MODE=nonstopmode
+# NOTE: pdftex uses its own path convention.
+#     - Entries are separated by ';'
+#     - Ending a path with '//' includes all subdirectories, recursively.
+#     - A path part of the form '{a,...,n}' will duplicate the current entry, once for each item in the list.
 
-# Search paths for the classes and packages included here.
+# User-defined path entries.
+# Override before including this file.
 PDFTEX_USER_PATH=
 
+# Default search directories
+# Includes all files in your project, and all classes and packages defined here.
 PDFTEX_PATH_LOCAL=.//
 PDFTEX_PATH_TEMPLATE=$(PDFTEX_DIR){classes,packages}
 PDFTEX_PATH_INTERNAL=$(PDFTEX_PATH_LOCAL);$(PDFTEX_PATH_TEMPLATE)
+
 PDFTEX_PATH=$(if $(PDFTEX_USER_PATH),$(PDFTEX_PATH_INTERNAL);$(PDFTEX_USER_PATH),$(PDFTEX_PATH_INTERNAL))
 
+# Compiler options
+
+# User-defined options for the compiler.
+# Override before including this file.
+PDFTEX_USER_FLAGS=
+
+# Interaction Mode for running PDFTEX
+#
+# This determines how LaTeX handles errors.
+# Depending on this mode, the compiler reacts to errrors
+# in one of the following ways:
+# - Abort compilation.
+# - Ask the user for help.
+# - Do nothing, and keep trying to process the file.
+#
+# There are two broad categories of error;
+# each mode will treat all errors of a given category the same.
+# The catgories are as follows:
+# - The compiler can't find a file.
+# - The compiler has a problem parsing or evaluating code.
+#
+# Options are as follows:
+# - errorstopmode:  Asks about all errors.
+# - scrollmode: 		Passes code errors, asks about missing files.
+# - nonstopmode:		Passes code errors, aborts on missing files.
+# - batchmode:			Passes code errors, aborts on missing files.  Doesn't log anything.
+#
+# By default, this variable is set to 'nonstopmode'.
+# This is the closest mode to a software compiler;
+# it will log all code errors, so you can address more than one bug at a time.
+# If you'd prefer one of the other modes, you can override this variable.
+PDFTEX_MODE=nonstopmode
+
+# Default options for the compiler:
+# - Set interaction mode based on PDFTEX_MODE
+# - Set the local search path (via the configuration variable 'TEXMFDOTDIR')
 PDFTEX_FLAGS_INTERNAL=-interaction=$(PDFTEX_MODE) -cnf-line='TEXMFDOTDIR = $(PDFTEX_PATH)'
 
 PDFTEX_FLAGS=$(if $(PDFTEX_USER_FLAGS),$(PDFTEX_FLAGS_INTERNAL) $(PDFTEX_USER_FLAGS),$(PDFTEX_FLAGS_INTERNAL))
 
-vpath %.tex src/tex
+# Compiler rules
+#
+# Because the LaTeX compiler manages its own dependencies,
+# it only needs the root-level '.tex' file to build the document.
 
+vpath %.tex src/tex
 
 .SECONDARY : %.aux
 %.aux : %.tex
